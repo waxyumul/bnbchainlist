a2614f9817216201721cf0efa38ece8a1d830eef
devinxl <94832688+devinxl@users.noreply.github.com>
2022-04-13 06:42:21 +0000

    Deploying to gh-pages from @ node-real/bnbchainlist@af362027ac44f723d4f6fd3574284f8e9e2ae50b 🚀

---
+++ _next/static/oGVii99NWWXNe0kiNaLcZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/338-cf8830b612b846d1.js","static/css/c6072e3184929ac4.css","static/chunks/pages/index-49bc6598665a96e9.js"],"/_error":["static/chunks/pages/_error-a3f18418a2205cb8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
